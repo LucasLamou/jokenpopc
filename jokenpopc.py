946ec57f27eb8cf28d59183ac57c2c20885de6a5
Lucas Lamounier <56306184+LucasLamou@users.noreply.github.com>
2019-10-18 12:12:35 -0300

    Add files via upload

--- jokenpopc.py
+++ jokenpopc.py
@@ -2,45 +2,45 @@ from random import randint
 from time import sleep
 itens=('predra', 'papel', 'tesoura')
 computador=randint(0, 2)
-print('o computador escolheu{}'.format(itens[computador]))
-print('''SUAS OPÇÕES: 
-[0]PEDRA
-[1]PAPEL
-[2]TESOURA''')
-jogador=int(input('FAÇA SUA JOGADA! '))
-print('JO')
+print('\033[36m'+'''SUAS OPÇÕES: 
+[0]PEDRA 
+[1]PAPEL 
+[2]TESOURA'''+'\033[0;0m')
+jogador=int(input('\033[36m'+'FAÇA SUA JOGADA! '+'\033[0;0m'))
+print('\033[31m'+'-=' * 33 +'\033[0;0m')
+print('\033[36m'+'JO'+'\033[0;0m')
 sleep(1)
-print('KEN')
+print('\033[36m'+'KEN'+'\033[0;0m')
 sleep(1)
-print('PÔ!!! ')
-print('-=' * 11)
-print('Computador jogou {}'.format(itens[computador]))
-print('Você jogou {}'.format(itens[jogador]))
-print('-=' * 11)
+print('\033[36m'+'PÔ!!!'+'\033[0;0m')
+print('\033[31m'+'-=' * 33 +'\033[0;0m')
+print('\033[36m'+'Computador jogou {}'.format(itens[computador]+'\033[0;0m'))
+print('\033[36m'+'Você jogou {}'.format(itens[jogador]+'\033[0;0m'))
+print('\033[31m'+'-=' * 33 +'\033[0;0m')
 if computador==0:
     if jogador==0:
-        print('DEU EMPATE! ')
+        print('\033[36m'+'DEU EMPATE! '+'\033[0;0m')
     elif jogador==1:
-        print('O VOCÊ VENCEU! ')
+        print('\033[36m'+'VOCÊ VENCEU! '+'\033[0;0m')
     elif jogador==2:
-        print('O COMPUTADOR VENCEU! ')
+        print('\033[36m'+'O COMPUTADOR VENCEU! '+'\033[0;0m')
     else:
-        print('JOGADA INVALIDA! ')
+        print('\033[36m'+'JOGADA INVALIDA! '+'\033[0;0m')
 elif computador==1:
     if jogador==0:
-        print('O COMPUTADOR VENCEU! ')
+        print('\033[36m'+'COMPUTADOR VENCEU! '+'\033[0;0m')
     elif jogador==1:
-        print('DEU EMPATE! ')
+        print('\033[36m'+'DEU EMPATE! '+'\033[0;0m')
     elif jogador==2:
-        print('O VOCÊ VENCEU! ')
+        print('\033[36m'+'VOCÊ VENCEU! '+'\033[0;0m')
     else:
-        print('JOGADA INVALIDA! ')
+        print('\033[36m'+'JOGADA INVALIDA! '+'\033[0;0m')
 elif computador==2:
     if jogador==0:
-        print('O VOCÊ VENCEU! ')
+        print('\033[36m'+'VOCÊ VENCEU! '+'\033[0,0m')
     elif jogador==1:
-        print('O COMPUTADOR VENCEU! ')
+        print('\033[36m'+'O COMPUTADOR VENCEU! '+'\033[0,0m')
     elif jogador==2:
-        print('DEU EMPATE! ')
+        print('\033[36m'+'DEU EMPATE! '+'\033[0;0m')
     else:
-        print('JOGADA INVALIDA! ')
+        print('\033[36m'+'JOGADA INVALIDA! '+'\033[0;0m')
